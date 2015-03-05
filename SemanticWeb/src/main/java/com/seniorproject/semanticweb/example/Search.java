e85f8c6e746bc80b37fbcb3953534f71a45e1792
Mimosha <mtmmoei@gmail.com>
2015-03-06 06:01:09 +0700

    query จากข้อมูลหนังแบบธรรมดาได้แล้ว แต่ web ยังไม่ได้

--- SemanticWeb/src/main/java/com/seniorproject/semanticweb/example/Search.java
+++ SemanticWeb/src/main/java/com/seniorproject/semanticweb/example/Search.java
@@ -5,42 +5,41 @@
  */
 package com.seniorproject.semanticweb.example;
 
+import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ReadWrite;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFactory;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.query.ResultSetRewindable;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.tdb.TDBLoader;
+import com.hp.hpl.jena.tdb.base.file.Location;
+import com.hp.hpl.jena.tdb.sys.TDBInternal;
 import com.hp.hpl.jena.util.FileManager;
+import java.io.InputStream;
 
 /**
  *
  * @author mtmmoei
  */
 public class Search {
-        public static void main(String[]args){
-            String queryString="SELECT ?resource \n" +
-"WHERE { ?resource movie:filmid ?uri.\n" +
-"?resource dc:title \"Forrest Gump\" .\n" +
-"}\n" +
-"ORDER BY ?resource ";
-//            String queryString = "SELECT ?name WHERE { \n" +
-//"        		   ?person foaf:mbox <mailto:alice@example.org> .  \n" +
-//"        		    ?person foaf:name ?name . \n" +
-//"        		}";
-            System.out.println("1 test Search");
-        FileManager.get().addLocatorClassLoader(Search.class.getClassLoader());
-        System.out.println("2");
-        
-        long heapSize = Runtime.getRuntime().totalMemory();
-         
-        //Print the jvm heap size.
-        System.out.println("Heap Size = " + heapSize);
-        Model model = FileManager.get().loadModel("data/linkedmdb-latest-dump.nt");
-        System.out.println("3");
-        String prefix = 
+    public static void main(String[] args) {
+        FileManager fm = FileManager.get();
+        fm.addLocatorClassLoader(Search.class.getClassLoader());
+        InputStream in = fm.open("data/linkedmdb-latest-dump.nt");
+
+        Location location = new Location ("target/TDB");
+
+        // Load some initial data
+        TDBLoader.load(TDBInternal.getBaseDatasetGraphTDB(TDBFactory.createDatasetGraph(location)), in, false);
+         String prefix = 
                 "PREFIX owl: <http://www.w3.org/2002/07/owl#> " +
                 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
@@ -53,27 +52,85 @@ public class Search {
                 "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
                 "PREFIX dc: <http://purl.org/dc/terms/> " +
                 "PREFIX movie: <http://data.linkedmdb.org/resource/movie/> ";
-        System.out.println("4");
-//        Model model = FileManager.get().loadModel("data/data.nt");
-//        String prefix = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ";
-//        String queryString="SELECT ?s ?p ?o " +
-//"WHERE { ?s ?o ?p .}" ;
-//        Model model = FileManager.get().loadModel("data/University20-clean2.nt");
-
-        System.out.println("5");
-        Query query = QueryFactory.create(prefix+queryString);
-        System.out.println("6");
-        QueryExecution qexec = QueryExecutionFactory.create(query, model);
-        System.out.println("7");
-        String out = "";
-        System.out.println("8");
+        String queryString = 
+           "SELECT ?resource \n" +
+"WHERE { ?resource movie:filmid ?uri.\n" +
+"?resource dc:title \"Forrest Gump\" .\n" +
+"}\n" +
+"ORDER BY ?resource ";
+          String out = "";
+        Dataset dataset = TDBFactory.createDataset(location);
+        dataset.begin(ReadWrite.READ);
         try {
-            ResultSetRewindable results = ResultSetFactory.makeRewindable(qexec.execSelect());
+            Query query = QueryFactory.create(prefix+queryString);
+            QueryExecution qexec = QueryExecutionFactory.create(query, dataset);
+            try {
+                 ResultSetRewindable results = ResultSetFactory.makeRewindable(qexec.execSelect());
             out = ResultSetFormatter.asText(results);
             results.reset();
+            } finally {
+                qexec.close();
+            }
         } finally {
-            qexec.close();
+            dataset.end();
         }
         System.out.println(out);
     }
+    
+    
+//        public static void main(String[]args){
+//            String queryString="SELECT ?resource \n" +
+//"WHERE { ?resource movie:filmid ?uri.\n" +
+//"?resource dc:title \"Forrest Gump\" .\n" +
+//"}\n" +
+//"ORDER BY ?resource ";
+////            String queryString = "SELECT ?name WHERE { \n" +
+////"        		   ?person foaf:mbox <mailto:alice@example.org> .  \n" +
+////"        		    ?person foaf:name ?name . \n" +
+////"        		}";
+//            String assemblerFile = "data/linkedmdb-latest-dump.nt" ;
+//  Dataset dataset = TDBFactory.assembleDataset(assemblerFile) ;
+//  dataset.begin(ReadWrite.READ) ;
+//  // Get model inside the transaction
+//  Model model = dataset.getDefaultModel() ;
+//  dataset.end() ;
+//        FileManager.get().addLocatorClassLoader(Search.class.getClassLoader());
+//        
+//        long heapSize = Runtime.getRuntime().totalMemory();
+//         
+//        //Print the jvm heap size.
+//        System.out.println("Heap Size = " + heapSize);
+//        Model model = FileManager.get().loadModel("data/linkedmdb-latest-dump.nt");
+//        System.out.println("3");
+//        String prefix = 
+//                "PREFIX owl: <http://www.w3.org/2002/07/owl#> " +
+//                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+//                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+//                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+//                "PREFIX oddlinker: <http://data.linkedmdb.org/resource/oddlinker/> " +
+//                "PREFIX map: <file:/C:/d2r-server-0.4/mapping.n3#> " +
+//                "PREFIX db: <http://data.linkedmdb.org/resource/> " +
+//                "PREFIX dbpedia: <http://dbpedia.org/property/> " +
+//                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
+//                "PREFIX dc: <http://purl.org/dc/terms/> " +
+//                "PREFIX movie: <http://data.linkedmdb.org/resource/movie/> ";
+////        Model model = FileManager.get().loadModel("data/data.nt");
+////        String prefix = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ";
+////        String queryString="SELECT ?s ?p ?o " +
+////"WHERE { ?s ?o ?p .}" ;
+////        Model model = FileManager.get().loadModel("data/University20-clean2.nt");
+//
+//        Query query = QueryFactory.create(prefix+queryString);
+//        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+//        String out = "";
+//        try {
+//            ResultSetRewindable results = ResultSetFactory.makeRewindable(qexec.execSelect());
+//            out = ResultSetFormatter.asText(results);
+//            results.reset();
+//        } finally {
+//            qexec.close();
+//        }
+//        System.out.println(out);
+//    }
 }
--- SemanticWeb/src/main/java/com/seniorproject/semanticweb/services/WebServices.java
+++ SemanticWeb/src/main/java/com/seniorproject/semanticweb/services/WebServices.java
@@ -5,16 +5,24 @@
  */
 package com.seniorproject.semanticweb.services;
 
+import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.query.ResultSetFactory;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.query.ResultSetRewindable;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.tdb.TDBLoader;
+import com.hp.hpl.jena.tdb.base.file.Location;
+import com.hp.hpl.jena.tdb.sys.TDBInternal;
 import com.hp.hpl.jena.util.FileManager;
+import com.seniorproject.semanticweb.example.ExampleTDB_02;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import org.springframework.stereotype.Service;
 
 /**
@@ -23,47 +31,52 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WebServices {
-        public String queryJena(String queryString){
-            System.out.println("1 test");
-        FileManager.get().addLocatorClassLoader(WebServices.class.getClassLoader());
-        System.out.println("2");
-        
-        long heapSize = Runtime.getRuntime().totalMemory();
-         
-        //Print the jvm heap size.
-        System.out.println("Heap Size = " + heapSize);
-//        Model model = FileManager.get().loadModel("data/linkedmdb-latest-dump.nt");
-//        System.out.println("3");
-//        String prefix = 
-//                "PREFIX owl: <http://www.w3.org/2002/07/owl#> " +
-//                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
-//                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
-//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-//                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
-//                "PREFIX oddlinker: <http://data.linkedmdb.org/resource/oddlinker/> " +
-//                "PREFIX map: <file:/C:/d2r-server-0.4/mapping.n3#> " +
-//                "PREFIX db: <http://data.linkedmdb.org/resource/> " +
-//                "PREFIX dbpedia: <http://dbpedia.org/property/> " +
-//                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
-//                "PREFIX dc: <http://purl.org/dc/terms/> " +
-//                "PREFIX movie: <http://data.linkedmdb.org/resource/movie/> ";
-        Model model = FileManager.get().loadModel("data/data.nt");
-        String prefix = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> ";
-        Query query = QueryFactory.create(prefix+queryString);
-        System.out.println("6");
-        QueryExecution qexec = QueryExecutionFactory.create(query, model);
-        System.out.println("7");
+
+    public String queryJena(String queryString) {
+        FileManager fm = FileManager.get();
+        fm.addLocatorClassLoader(ExampleTDB_02.class.getClassLoader());
+        System.out.println("-1");
+        InputStream in = fm.open("data/linkedmdb-latest-dump.nt");
+System.out.println("0");
+        Location location = new Location("target/TDB");
+System.out.println("1");
+        // Load some initial data
+        TDBLoader.load(TDBInternal.getBaseDatasetGraphTDB(TDBFactory.createDatasetGraph(location)), in, false);
+System.out.println("2");
+        String prefix
+                = "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
+                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
+                + "PREFIX oddlinker: <http://data.linkedmdb.org/resource/oddlinker/> "
+                + "PREFIX map: <file:/C:/d2r-server-0.4/mapping.n3#> "
+                + "PREFIX db: <http://data.linkedmdb.org/resource/> "
+                + "PREFIX dbpedia: <http://dbpedia.org/property/> "
+                + "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> "
+                + "PREFIX dc: <http://purl.org/dc/terms/> "
+                + "PREFIX movie: <http://data.linkedmdb.org/resource/movie/> ";
+
         String out = "";
+        Dataset dataset = TDBFactory.createDataset(location);
+        System.out.println("3");
+        dataset.begin(ReadWrite.READ);
+        System.out.println("4");
         try {
-            ResultSetRewindable results = ResultSetFactory.makeRewindable(qexec.execSelect());
-             ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ResultSetFormatter.outputAsJSON(bos, results);
-            out = bos.toString();
-            results.reset();
+            Query query = QueryFactory.create(prefix + queryString);
+            QueryExecution qexec = QueryExecutionFactory.create(query, dataset);
+            try {
+                ResultSetRewindable results = ResultSetFactory.makeRewindable(qexec.execSelect());
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                ResultSetFormatter.outputAsJSON(bos, results);
+                results.reset();
+            } finally {
+                qexec.close();
+            }
         } finally {
-            qexec.close();
+            dataset.end();
         }
-        System.out.print(out);
+        System.out.println(out);
         return out;
     }
         
